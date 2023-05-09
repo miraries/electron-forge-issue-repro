ac620438c6e382b7130f6761dba20a814c0f767e
Ivan Kotlaja <ivan.kotlaja@igamingplatform.com>
2023-05-09 21:43:38 +0200

    add & require test.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+require('./test');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
